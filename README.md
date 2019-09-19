35ed867fe75a4a0bd1742cc37e129f09ffc0762e
Graeme Woods <graemewoods202@gmail.com>
2019-09-19 01:05:59 -0400

    tech site

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Personal Website
-Both tech and media websites are stored in separate branches.
-
 Design by HTML5 UP
-
-By Graeme
--- assets/js/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $("#myWork").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#main").offset().top
-    }, 900);
-  });
-});
